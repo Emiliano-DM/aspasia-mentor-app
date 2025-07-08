0e396f1a136eb41a1f87c6f568e6773301450efe
emiliano <e.dimascolo2024@gmail.com>
2025-07-08 14:15:05 +0200

    change API

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000/api";
+const API = "https://idea-de-txabi.onrender.com/api";
 
 export async function fetchData(endpoint) {
   const response = await fetch(`${API}/${endpoint}`);
