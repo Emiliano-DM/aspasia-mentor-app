88e274a67ad72ad0f5b1d4b6f3493bd607c22f4d
emiliano <e.dimascolo2024@gmail.com>
2025-07-08 13:54:49 +0200

    pagina de resultado sin css

---
+++ src/components/ResultRecord/ResultRecord.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ResultRecord.css"; // Assuming you have a CSS file for styling
+
+function ResultRecord({ data, onClick }) {
+  const { teams, mentors } = data;
+
+  return (
+    <div id="result-record-container" onClick={onClick}>
+      <div className="mentor-info">
+        <p>{mentors.nombre}</p>
+        <p>{mentors.empresa}</p>
+      </div>
+      <div className="group-info">
+        <p>Group: {teams.nombre}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ResultRecord;
